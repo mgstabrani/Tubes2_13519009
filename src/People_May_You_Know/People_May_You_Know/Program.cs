d59cf48393407a5e6dca04f0f0131c6abdacb85c
yudialfayat <ayudi3521@gmail.com>
2021-03-24 09:15:25 +0700

    Bagian 4 6 7

---
+++ src/People_May_You_Know/People_May_You_Know/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace People_May_You_Know
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new main());
+        }
+    }
+}
